519f712d156c74c5c6008a3c1d06330753b8187c
redsylx <fakhrialbari.red@gmail.com>
2024-09-26 23:59:50 +0700

    basic svelte 1

---
+++ src/components/Card.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let title : string;
+  export let text : string;
+</script>
+
+<div class="card">
+  <p class="card-title">{title}</p>
+  <p class="card-text">{text}</p>
+</div>
+
+<style>
+  .card {
+    background-color: hsl(0, 80%, 5%);
+    padding: 20px;
+    border: 2px solid hsl(0, 80%, 40%);
+    justify-content: space-between;
+  }
+
+  .card-title {
+    font-family: 'Helvetica';
+    color: hsl(0, 80%, 60%);
+    padding: 0px;
+    margin: 0px 0px 8px 0px;
+  }
+
+  .card-text {
+    font-family: 'Helvetica';
+    color: hsl(0, 80%, 80%);
+    padding: 0px;
+    margin: 0px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Card from "../components/Card.svelte";
+
+  let name: string = "redsylx";
+
+  let multiplier: number = 1;
+  let number: number = 1;
+
+  $: result = multiplier * number;
+  
+  $: console.log(`multiplier ${multiplier}`);
+  $: console.log(`number     ${number}`);
+</script>
+
+<p class="title">Welcome to SvelteKit</p>
+<p class="name">{name}</p>
+<p class="text">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Card text="this is only sample card" title="sample"/>
+<input bind:value={multiplier}>
+<input bind:value={number}>
+<p class="text">{result}</p>
+<style>
+  :root {
+    background-color: hsl(0, 80%, 5%);
+    padding: 20px 40px;
+  }
+
+  .title {
+    font-size: large;
+    font-family: 'Helvetica';
+    font-weight: 600;
+    color: hsl(0, 80%, 60%);
+  }
+
+  .name {
+    font-size: medium;
+    font-family: 'Helvetica';
+    font-weight: 900;
+    color: hsl(0, 80%, 40%);
+  }
+
+  .text {
+    font-size: small;
+    font-family: 'Helvetica';
+    font-weight: 300;
+    color: hsl(0, 80%, 80%);
+  }
+</style>
